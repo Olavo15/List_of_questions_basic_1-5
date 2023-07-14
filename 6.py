7ed828498b4265876523370f83ed940095ba8dff
olavo15 <olavoregis169@gmail.com>
2023-07-14 10:59:29 -0300

    first commit

---
+++ 6.py
@@ -0,0 +1,12 @@
+"""
+Leia uma temperatura em graus celsius e apresente-a 
+convertida em graus Fahrenheit.A fórmula de conversão é:
+F=C*(9.0/5.0)+32.0, sendo F a temperaturaem Fahrenheit e C
+a temperatura em Celsius.    
+"""
+
+c = float(input())
+
+f = c * (9.0 / 5.0) + 32.0
+
+print(f)
---
+++ 7.py
@@ -0,0 +1,10 @@
+"""
+Leia uma temperatura em graus Fahrenheit e apresente-a convertida em graus Celsius.
+A formula de conversão é: C = 5.0 * ( F - 32.0) / 9.0,sendo C a temperatura em Celsius e F a temperatura em Fahrenheit.
+"""
+
+f = float(input())
+
+C = 5 * (f - 32 ) / 9
+
+print(C)
